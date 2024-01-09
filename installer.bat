efda87c7c1cade883d3ad119203caa411a2456cf
Storterald <storterald@gmail.com>
2024-01-09 22:16:22 +0100

    update

---
+++ installer.bat
@@ -0,0 +1,5 @@
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+@echo off
+title Extension installer
+code --install-extension intellij-idea-theme-for-vs-code-1.0.0.vsix
