0b6db1bcd3861d7e6ddde71596a91af35154dd82
Storterald <storterald@gmail.com>
2024-09-09 22:27:46 +0200

    license now txt file + now build script also fetches and builds converter when required

--- build.py
+++ build.py
@@ -1,6 +1,7 @@
 import os
 import re
 import sys
+import stat
 import shutil
 import subprocess
 
@@ -15,13 +16,17 @@ if __name__ == "__main__":
         subprocess.run(["code", "--install-extension", FILE], shell=True)
 
         if len(sys.argv) > 1 and sys.argv[1] == "-C":
+                subprocess.run(["git", "clone", "https://github.com/microsoft/theme-converter-for-vs", "--branch", "main", "--single-branch", "./theme-converter"])
+                subprocess.run(["dotnet", "build"], cwd="./theme-converter/ThemeConverter/ThemeConverter", shell=True)
+
                 EXECUTABLE_DIR: str = "../theme-converter/ThemeConverter/ThemeConverter/bin/debug/net6.0"
                 PATH: str = os.path.abspath("./themes")
                 
                 # Change working directory
                 os.chdir(PATH)
 
-                for file in [file for file in os.listdir("./") if file.endswith(".json")]:
+                FILES: list[str] = [file for file in os.listdir("./") if file.endswith(".json")]
+                for file in FILES:
                         OUTPUT_FILE: str = Path(file).stem[:-len("-color-theme")] + ".pkgdef"
 
                         if os.path.exists(OUTPUT_FILE):
@@ -47,3 +52,15 @@ if __name__ == "__main__":
                         # Remove tmp- prefix and -color-theme suffix and delete file
                         os.rename(f"tmp-{Path(OUTPUT_FILE).stem}-color-theme.pkgdef", OUTPUT_FILE)
                         os.remove(f"tmp-{file}")
+                
+                # Remove cloned repo
+                def onerror(func, path: str, _):
+                        if not os.access(path, os.W_OK):
+                                # Change file permission
+                                os.chmod(path, stat.S_IWUSR)
+                                func(path)
+                        else:
+                                # If error is not due to permission issues, raise
+                                assert False, "Could not delete cloned directory."
+
+                shutil.rmtree("../theme-converter", onexc=onerror)
