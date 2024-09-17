14b5d5e38ff235ef744a8fd0827c57b3dde25d2b
Storterald <storterald@gmail.com>
2024-09-17 15:10:26 +0200

    improved build system + fixed git and vscode ignores

--- build.py
+++ build.py
@@ -36,56 +36,61 @@ def getFileData(path: str) -> str:
 
         return json.dumps(getJsonData(path), indent=2)
 
+def convert() -> None:
+        subprocess.run(["git", "clone", "https://github.com/microsoft/theme-converter-for-vs", "--branch", "main", "--single-branch", "./theme-converter"])
+        subprocess.run(["dotnet", "build"], cwd="./theme-converter/ThemeConverter/ThemeConverter", shell=True)
+
+        EXECUTABLE_DIR: str = "../theme-converter/ThemeConverter/ThemeConverter/bin/debug/net6.0"
+        PATH: str = os.path.abspath("./vsthemes/")
+        THEMES_PATH: str = os.path.abspath("./themes/")
+        
+        # Change working directory
+        os.chdir(PATH)
+
+        # Create complete JSON files
+        for file in [file for file in os.listdir(THEMES_PATH) if file.endswith(".json")]:
+                # Delete converted theme if it exists
+                OUTPUT_FILE: str = Path(file).stem + ".pkgdef"
+                if os.path.exists(OUTPUT_FILE):
+                        os.remove(OUTPUT_FILE)
+
+                # Get full JSON data without comments
+                with open(f"./{Path(file).stem}.json", 'w', encoding="utf-8") as f:
+                        f.write(getFileData(f"{THEMES_PATH}/{file}"))
+        
+        for file in [file for file in os.listdir("./") if file.endswith(".json")]:
+                # Convert all themes
+                subprocess.run(["ThemeConverter.exe", "-i", f"{PATH}/{file}", "-o", PATH], cwd=EXECUTABLE_DIR, shell=True)
+
+                if not os.path.exists(f"{PATH}/{file}"):
+                        assert False, "Error creating converted file"
+
+                # Delete temporary JSONs
+                os.remove(file)
+        
+        # Remove cloned repo
+        def onerror(func, path: str, _):
+                if not os.access(path, os.W_OK):
+                        # Change file permission
+                        os.chmod(path, stat.S_IWUSR)
+                        func(path)
+                else:
+                        # If error is not due to permission issues, raise
+                        assert False, "Could not delete cloned directory."
+
+        # Delete cloned converter
+        shutil.rmtree("../theme-converter/", onexc=onerror)
+
 if __name__ == "__main__":
         # Compile extension
-        subprocess.run(["vsce", "package"], shell=True)
-
-        # Install the extension
-        FILE: str = [path for path in os.listdir("./") if path.startswith("jetbrains-themes-for-vs-code") and path.endswith(".vsix")][0] # Not specifying version
-        subprocess.run(["code", "--install-extension", FILE], shell=True)
-
-        if len(sys.argv) > 1 and sys.argv[1] == "-C":
-                subprocess.run(["git", "clone", "https://github.com/microsoft/theme-converter-for-vs", "--branch", "main", "--single-branch", "./theme-converter"])
-                subprocess.run(["dotnet", "build"], cwd="./theme-converter/ThemeConverter/ThemeConverter", shell=True)
-
-                EXECUTABLE_DIR: str = "../theme-converter/ThemeConverter/ThemeConverter/bin/debug/net6.0"
-                PATH: str = os.path.abspath("./themes")
+        subprocess.run(["vsce", "package"], shell=True)        
+        
+        # Run through all the flags
+        for flag in sys.argv[1:]:
+                match flag:
+                        case "-I":
+                                FILE: str = [path for path in os.listdir("./") if path.endswith(".vsix")][0]
+                                subprocess.run(["code", "--install-extension", FILE], shell=True)
+                        case "-C":
+                                convert()
                 
-                # Change working directory
-                os.chdir(PATH)
-
-                FILES: list[str] = [file for file in os.listdir("./") if file.endswith(".json")]
-                for file in FILES:              
-                        OUTPUT_FILE: str = Path(file).stem[:-len("-color-theme")] + ".pkgdef"
-
-                        # Clear old converted theme
-                        if os.path.exists(OUTPUT_FILE):
-                                os.remove(OUTPUT_FILE)
-
-                        data: str = getFileData(file)                             
-
-                        # Create fixed temporary file
-                        with open(f"tmp-{file}", 'w', encoding="utf-8") as f:
-                                f.write(data)
-
-                        subprocess.run(["ThemeConverter.exe", "-i", f"{PATH}/tmp-{file}", "-o", f"{PATH}/"], cwd=EXECUTABLE_DIR, shell=True)
-                        if os.path.exists(f"tmp-{Path(OUTPUT_FILE).stem}-color-theme.pkgdef"):
-                                print(f"Successfully converted {file} in VS theme.")
-                        else:
-                                assert False, f"Error converting {file}"
-
-                        # Remove tmp- prefix and -color-theme suffix and delete file
-                        os.rename(f"tmp-{Path(OUTPUT_FILE).stem}-color-theme.pkgdef", OUTPUT_FILE)
-                        os.remove(f"tmp-{file}")
-                
-                # Remove cloned repo
-                def onerror(func, path: str, _):
-                        if not os.access(path, os.W_OK):
-                                # Change file permission
-                                os.chmod(path, stat.S_IWUSR)
-                                func(path)
-                        else:
-                                # If error is not due to permission issues, raise
-                                assert False, "Could not delete cloned directory."
-
-                shutil.rmtree("../theme-converter", onexc=onerror)
