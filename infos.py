f4f46ae68730326a42bbfe943ef5d951ca4dff6b
Storterald <storterald@gmail.com>
2025-03-28 20:02:38 +0100

    renamed extension + added package lock to gitignore

--- infos.py
+++ infos.py
@@ -1,3 +1,3 @@
 VERSION_DIR: str = "./.version-getter/"
-GITHUB_REPO: str = "https://github.com/Storterald/VSCode-Profiler-Integration"
-NAME: str = "jetbrains-themes"
+GITHUB_REPO: str = "https://github.com/Storterald/vscode-jetbrains-theme"
+NAME: str = "vscode-jetbrains-theme"
